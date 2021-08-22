fe0531e5ad1b954bb1a4e55fb1ca61f4a57c494c
Patrick <zhen.patrick@gmail.com>
2021-08-22 17:24:21 -0400

    connected backend with frontend

--- src/app/services/task-service.service.ts
+++ src/app/services/task-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http"
 import {Observable, of } from 'rxjs'
 import { Task } from '../Task';
 import { TASKS } from '../mock-data';
@@ -7,11 +8,11 @@ import { TASKS } from '../mock-data';
   providedIn: 'root'
 })
 export class TaskServiceService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable <Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
