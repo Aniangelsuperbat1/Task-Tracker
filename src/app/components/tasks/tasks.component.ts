95b2587d387c408130e9da545e95c0b078d892ae
Patrick <zhen.patrick@gmail.com>
2021-08-25 11:14:51 -0400

    new tasks can be added

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -33,6 +33,8 @@ export class TasksComponent implements OnInit {
   }
 
   addTask(task: Task){
-    console.log(task)
+    this.taskService.addTask(task).subscribe((task) => {
+      this.tasks.push(task);
+    });
   }
 }
